0997359070312e4a41ab119cefab42296ea324ad
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-11-07 11:26:02 +0200

    Changed working file

--- print-info-from-file.js
+++ print-info-from-file.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
 const readline = require('readline');
 
-let count = 0;
+const countries = new Map();
+
+const countriesCount = (country) => {
+    let countryCount = countries.get(country);
+    if (countryCount) {
+        countryCount++
+        countries.set(country, countryCount);
+    }else{
+        countries.set(country, 1);
+    }
+}
+
 
 const stream = readline.createInterface({
-    input: fs.createReadStream('countries-list.txt')
-});
-
-stream.on('line', line => {
-    count++
-    console.log(`${count}: ${line}`);
+    input: fs.createReadStream('free_company_dataset.csv')
 });
 
 stream.on('error', error => {
     console.error(error);
 });
+
+stream.on('line', line => {
+    const country = line.split(',')[0]
+    countriesCount(country);
+});
+
+stream.on('close', () => {
+    console.log(countries);
+})
