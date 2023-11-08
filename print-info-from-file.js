5253d90b7cabd425863334dac1badc10aa91dae2
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-11-08 14:44:23 +0200

    Changed Map() to object

--- print-info-from-file.js
+++ print-info-from-file.js
@@ -1,18 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const countries = new Map();
-
-const countriesCount = (country) => {
-    let countryCount = countries.get(country);
-    if (countryCount) {
-        countryCount++
-        countries.set(country, countryCount);
-    }else{
-        countries.set(country, 1);
-    }
-}
-
+const countries = {};
 
 const stream = readline.createInterface({
     input: fs.createReadStream('free_company_dataset.csv')
@@ -24,7 +13,7 @@ stream.on('error', error => {
 
 stream.on('line', line => {
     const country = line.split(',')[0]
-    countriesCount(country);
+    countries[country] = countries[country] ? countries[country] + 1 : 1;
 });
 
 stream.on('close', () => {
